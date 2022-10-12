348c969ad232a791e2e5bfd67fbb2fa5517bb332
AlejandraAAO <alejandra.amezaga.aao@gmail.com>
2022-10-11 21:00:15 -0500

    cap1 react begiiners

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import './index.css';
+// {} podria exportar cosas de un solo archivo
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+//////////////////////Creamos un elemento root 
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+
+//metodo render: espera hijos/ nuestra app
+rootElement.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
